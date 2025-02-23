80490b808ed0301450fe86cd8b1021b9c363fe85
Rendobro <160195334+Rendobro@users.noreply.github.com>
2025-02-22 23:11:18 -0500

    Level Select, Bug Fixes, Menu Crazy Double Revamp!

--- Assets/Scripts/ButtonLoaderScript.cs
+++ Assets/Scripts/ButtonLoaderScript.cs
@@ -1,88 +1,146 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class ButtonLoaderScript : MonoBehaviour
 {
-    public float menuTransitionDuration = 1.6f;
-    private int currentMenuIndex = 1;
-    private GameObject[] menus;
+    [SerializeField] private float menuTransitionDuration = 1.6f;
+    private static int currentMenuIndex_X = 1;
+    private static int currentMenuIndex_Y = 1;
+    private GameObject[][] menus;
     private CharacterController player;
     private PlayerResetScript prs;
-    private bool[] readyToMove;
+    private TextMeshProUGUI playText;
+    private bool[][] readyToMove;
     // Start is called before the first frame update
     void Start()
     {
-        menus = GameObject.FindGameObjectsWithTag("MenuBoard");
-        System.Array.Sort(menus, (x, y) => GetMenuIndex(x).CompareTo(GetMenuIndex(y)));
-        {
-            readyToMove = new bool[menus.Length];
-            for (int i = 0; i<readyToMove.Length;i++)
-                readyToMove[i] = true;
-        }
-        for (int i = 0; i<menus.Length;i++)
-        {
-            Slider[] valueSlider = menus[i].GetComponentsInChildren<Slider>();
-            for (int j = 0; j<valueSlider.Length;j++)
-            {
-                string nameOfSlider = valueSlider[j].gameObject.name.Substring(0,valueSlider[j].gameObject.name.Length-6);
-                float value = PlayerPrefs.GetFloat(nameOfSlider.ToLower());
-                valueSlider[j].value = value;
-                menus[i].transform.Find(nameOfSlider+"Value").GetComponent<TextMeshProUGUI>().text = Mathf.Round(value).ToString();
-                if (nameOfSlider.Equals("Sensitivity"))
-                {
-                    valueSlider[j].onValueChanged.AddListener(SetSensitivity);
-                }
-                else if (nameOfSlider.Equals("Gravity"))
-                {
-                    valueSlider[j].onValueChanged.AddListener(SetGravity);
-                }
-            }
-        }
+        playText = GameObject.Find("MenuBoard11").GetComponentInChildren<Button>().gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        InitializeMenus();
+        InitializeSliders();
     }
-    public void LoadCorrectScene()
+    void Update()
     {
-        if (PlayerPrefs.GetInt("levelIndex",0)<1) 
-        {
-            PlayerPrefs.SetInt("levelIndex",1);
-        }
-        SceneManager.LoadScene(PlayerPrefs.GetInt("levelIndex"));
-        SceneManager.sceneLoaded += LoadCheckpointData;
+        menuTransitionDuration = PlayerPrefs.GetFloat("menuspeed");
     }
     public void BringSettingsMenuLeft()
     {
-        Debug.Log(readyToMove);
-        Debug.Log(menus == null);
+        bool incrementOkay = true;
         for (int i = 0;i<menus.Length;i++)
         {
-            if (readyToMove[i])
+            for (int j = 0;j<menus[i].Length;j++)
             {
-                readyToMove[i] = false;
-                Transform[] transforms = menus[i].GetComponentsInChildren<Transform>();
-                Transform mlp = transforms.FirstOrDefault(t => t.gameObject.name.Equals("MenuLeftPosition"+(i+1)));
-                
-                StartCoroutine(SlowMovePosition(menus[i].transform.position,mlp.position,menuTransitionDuration,i));
+                if (readyToMove[i][j])
+                {
+                    Transform _t = menus[i][j].transform;
+                    readyToMove[i][j] = false;
+                    Transform[] transforms = menus[i][j].GetComponentsInChildren<Transform>();
+                    Transform mlp = transforms.FirstOrDefault(t => t.gameObject.name.Equals("MenuLeftPosition"+(i+1)+(j+1)));
+                    
+                    StartCoroutine(SlowMovePosition(_t.position,mlp.position,menuTransitionDuration,(i,j)));
+                }
+                else
+                {
+                    incrementOkay = false;
+                }
             }
         }
-        currentMenuIndex++;
+        if (incrementOkay)
+        currentMenuIndex_X++;
+    }
+    public void BringSettingsMenuUp()
+    {
+        bool incrementOkay = true;
+        for (int i = 0;i<menus.Length;i++)
+        {
+            for (int j = 0;j<menus[i].Length && menus[i][j] != null;j++)
+            {
+                if (readyToMove[i][j])
+                {
+                    Transform _t = menus[i][j].transform;
+                    readyToMove[i][j] = false;
+                    Transform[] transforms = menus[i][j].GetComponentsInChildren<Transform>();
+                    Transform mup = transforms.FirstOrDefault(t => t.gameObject.name.Equals("MenuUpPosition"+(i+1)+(j+1)));
+                    
+                    StartCoroutine(SlowMovePosition(_t.position, mup.position, menuTransitionDuration,(i,j)));
+                }
+                else
+                {
+                    incrementOkay = false;
+                }
+            }
+        }
+        if (incrementOkay)
+        currentMenuIndex_Y++;
+    }
+    public void BringSettingsMenuDown()
+    {
+        bool incrementOkay = true;
+        for (int i = 0;i<menus.Length;i++)
+        {
+            for (int j = 0;j<menus[i].Length;j++)
+            {
+                if (readyToMove[i][j])
+                {
+                    Transform _t = menus[i][j].transform;
+                    readyToMove[i][j] = false;
+                    Transform[] transforms = menus[i][j].GetComponentsInChildren<Transform>();
+                    Transform mdp = transforms.FirstOrDefault(t => t.gameObject.name.Equals("MenuDownPosition"+(i+1)+(j+1)));
+                    
+                    StartCoroutine(SlowMovePosition(_t.position,mdp.position,menuTransitionDuration,(i,j)));
+                }
+                else
+                {
+                    incrementOkay = false;
+                }
+            }
+        }
+        if (incrementOkay)
+        currentMenuIndex_Y--;
     }
     public void BringSettingsMenuRight()
     {
-        Debug.Log(readyToMove);
-        Debug.Log(menus == null);
+        bool incrementOkay = true;
         for (int i = 0;i<menus.Length;i++)
         {
-            if (readyToMove[i])
+            for (int j = 0;j<menus[i].Length;j++)
             {
-                readyToMove[i] = false;
-                Transform[] transforms = menus[i].GetComponentsInChildren<Transform>();
-                Transform mrp = transforms.FirstOrDefault(item => item.gameObject.name.Equals("MenuRightPosition"+(i+1)));
-                StartCoroutine(SlowMovePosition(menus[i].transform.position,mrp.position,menuTransitionDuration,i));
+                if (readyToMove[i][j])
+                {
+                    Transform _t = menus[i][j].transform;
+                    readyToMove[i][j] = false;
+                    Transform[] transforms = menus[i][j].GetComponentsInChildren<Transform>();
+                    Transform mrp = transforms.FirstOrDefault(t => t.gameObject.name.Equals("MenuRightPosition"+(i+1)+(j+1)));
+                    
+                    StartCoroutine(SlowMovePosition(_t.position,mrp.position,menuTransitionDuration,(i,j)));
+                }
+                else
+                {
+                    incrementOkay = false;
+                }
             }
         }
-        currentMenuIndex--;
+        if (incrementOkay)
+        currentMenuIndex_X--;
+    }
+    public void LoadCorrectScene()
+    { 
+        if (PlayerPrefs.GetInt("levelIndex",0)<1) PlayerPrefs.SetInt("levelIndex",1);
+        SceneManager.sceneLoaded -= LoadCheckpointData;
+        SceneManager.sceneLoaded -= ChangePlayText;
+        SceneManager.sceneLoaded += LoadCheckpointData;
+        SceneManager.sceneLoaded += ChangePlayText;
+        SceneManager.LoadScene(PlayerPrefs.GetInt("levelIndex"));
+    }
+    private void ChangePlayText(Scene scene, LoadSceneMode mode)
+    {
+        playText.text = "Continue Playing!";
+        playText.fontSize = 20;
     }
     private void LoadCheckpointData(Scene scene, LoadSceneMode mode)
     {
@@ -106,49 +164,126 @@ public class ButtonLoaderScript : MonoBehaviour
             }
         }
     }
-    private IEnumerator SlowMovePosition(Vector3 startPosition, Vector3 endPosition, float duration, int index)
+    private IEnumerator SlowMovePosition(Vector3 startPosition, Vector3 endPosition, float duration, (int row, int col) indexCoords)
     {
+        Transform _t = menus[indexCoords.row][indexCoords.col].transform;
         float elapsedTime = 0;
         while (elapsedTime < duration)
         {
             elapsedTime += Time.deltaTime;
             float t = elapsedTime / duration;
             float easedT = t < 0.5f ? 2 * t * t : -1 + (4 - 2 * t) * t;
-            menus[index].transform.position = Vector3.Lerp(startPosition, endPosition, easedT);
+            _t.position = Vector3.Lerp(startPosition, endPosition, easedT);
             yield return null;
         }
-        menus[index].transform.position = endPosition;
-        readyToMove[index] = true;
+        _t.position = endPosition;
+        readyToMove[indexCoords.row][indexCoords.col] = true;
     }
     public void SetSensitivity(float value)
     {
-        Slider sensitivitySlider = menus[currentMenuIndex-1].transform.Find("SensitivitySlider").gameObject.GetComponent<Slider>();
+        Slider sensitivitySlider = menus[currentMenuIndex_Y-1][currentMenuIndex_X-1].transform.Find("SensitivitySlider").gameObject.GetComponent<Slider>();
         float sensitivityValue = PlayerPrefs.GetFloat("sensitivity");
-        TextMeshProUGUI sensitivityText = menus[currentMenuIndex-1].transform.Find("SensitivityValue").gameObject.GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI sensitivityText = menus[currentMenuIndex_Y-1][currentMenuIndex_X-1].transform.Find("SensitivityValue").gameObject.GetComponent<TextMeshProUGUI>();
 
         ClampValues(sensitivityValue,sensitivitySlider.minValue,sensitivitySlider.maxValue);
         PlayerPrefs.SetFloat("sensitivity", value);
 
         sensitivityText.text = Mathf.Round(value).ToString();
     }
-    private void ClampValues(float value, float min, float max)
-    {
-        value = value < min ? min : value;
-        value = value > max ? max : value;
-    }
     public void SetGravity(float value)
     {
-        Slider gravitySlider = menus[currentMenuIndex-1].transform.Find("GravitySlider").gameObject.GetComponent<Slider>();
+        Slider gravitySlider = menus[currentMenuIndex_Y-1][currentMenuIndex_X-1].transform.Find("GravitySlider").gameObject.GetComponent<Slider>();
         float gravityValue = PlayerPrefs.GetFloat("sensitivity");
-        TextMeshProUGUI gravityText = menus[currentMenuIndex-1].transform.Find("GravityValue").gameObject.GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI gravityText = menus[0][currentMenuIndex_X-1].transform.Find("GravityValue").gameObject.GetComponent<TextMeshProUGUI>();
         
         ClampValues(gravityValue,gravitySlider.minValue,gravitySlider.maxValue);
         PlayerPrefs.SetFloat("gravity", value);
         
         gravityText.text = Mathf.Round(value).ToString();
     }
-    private static int GetMenuIndex(GameObject menu)
+
+    public void SetMenuSpeed(float value)
     {
-        return int.Parse(menu.name[^1]+"");
+        Slider menuSpeedSlider = menus[currentMenuIndex_Y-1][currentMenuIndex_X-1].transform.Find("MenuSpeedSlider").gameObject.GetComponent<Slider>();
+        float menuSpeedValue = PlayerPrefs.GetFloat("menuspeed");
+        TextMeshProUGUI menuSpeedText = menus[currentMenuIndex_Y-1][currentMenuIndex_X-1].transform.Find("MenuSpeedValue").gameObject.GetComponent<TextMeshProUGUI>();
+        
+        ClampValues(menuSpeedValue,menuSpeedSlider.minValue,menuSpeedSlider.maxValue);
+        PlayerPrefs.SetFloat("menuspeed", value);
+        
+        menuSpeedText.text = value.ToString("F2");
+    }
+    private void InitializeMenus()
+    {
+        GameObject[] allMenus = GameObject.FindGameObjectsWithTag("MenuBoard");
+        int maxRow = allMenus.Max(m => GetMenuIndex(m).row);
+        (int row,int col)[] rowsAndCols = allMenus.Select(m => GetMenuIndex(m)).ToArray();
+        Dictionary<int, int> rowColumnCounts = new();
+        for (int i = 0; i<allMenus.Length;i++)
+        {
+            int currentRow = rowsAndCols[i].row;
+
+            if (rowColumnCounts.ContainsKey(currentRow)) rowColumnCounts[currentRow]++;
+            else rowColumnCounts.Add(currentRow,1);
+        }
+        menus = new GameObject[maxRow][];
+        foreach (var kvp in rowColumnCounts)
+        {
+            menus[kvp.Key - 1] = new GameObject[kvp.Value];
+        }
+        readyToMove = new bool[maxRow][];
+        for (int i = 0; i<maxRow;i++)
+        {
+            int itemsInRow = rowColumnCounts[i+1];
+            readyToMove[i] = new bool[itemsInRow];
+            for (int j = 0; j<itemsInRow;j++)
+            {
+                readyToMove[i][j] = true;
+            }
+        }
+        for (int i = 0; i<allMenus.Length;i++)
+        {
+            (int currentRow,int currentCol) = rowsAndCols[i];
+            menus[currentRow-1][currentCol-1] = allMenus[i];
+        }
+    }
+    private void InitializeSliders()
+    {
+        for (int i = 0; i<menus.Length;i++)
+        {
+            for (int k = 0; k<menus[i].Length;k++)
+            {
+                Slider[] valueSlider = menus[i][k].GetComponentsInChildren<Slider>();
+            for (int j = 0; j<valueSlider.Length;j++)
+            {
+                string nameOfSlider = valueSlider[j].gameObject.name.Substring(0,valueSlider[j].gameObject.name.Length-6);
+                float value = PlayerPrefs.GetFloat(nameOfSlider.ToLower());
+                valueSlider[j].value = value;
+                menus[i][k].transform.Find(nameOfSlider+"Value").GetComponent<TextMeshProUGUI>().text = nameOfSlider.Equals("MenuSpeed")? value.ToString("F2"): Mathf.Round(value).ToString();
+                
+                if (nameOfSlider.Equals("Sensitivity"))
+                {
+                    valueSlider[j].onValueChanged.AddListener(SetSensitivity);
+                }
+                else if (nameOfSlider.Equals("Gravity"))
+                {
+                    valueSlider[j].onValueChanged.AddListener(SetGravity);
+                }
+                else if (nameOfSlider.Equals("MenuSpeed"))
+                {
+                    valueSlider[j].onValueChanged.AddListener(SetMenuSpeed);
+                }
+            }
+            }
+        }
+    }
+    private void ClampValues(float value, float min, float max)
+    {
+        value = value < min ? min : value;
+        value = value > max ? max : value;
+    }
+    private static (int row, int col) GetMenuIndex(GameObject menu)
+    {
+        return (int.Parse(menu.name[^2]+""),int.Parse(menu.name[^1]+""));
     }
 }
